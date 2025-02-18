db8d5cf8f3228faa4a9c630cee4e9b9a7266ee7b
gelien0901 <201980292@psu.palawan.edu.ph>
2025-02-18 12:51:43 +0800

    Update questions

--- php practice test.php
+++ php practice test.php
@@ -18,7 +18,19 @@ var_dump($person)
 var_dump($Hello! Charlene);
 var_dump([1,2,3]);
 
-
+#3. What is the output of var_dump($cashOnHand);?
+#4. What character is used to concatenate strings?
+#5. What is the output of echo HOST?
+#6. How will you define in PHP the variable PI with value 3.1416?
+#7. What is the output of echo $numbers[3] + $numbers[4];?
+#8. What is the output of echo $colors[30];?
+#9. What are the keys in $person array?
+#10. In PHP, how will you change the green into white in $colors array?
+#11. What is the output of echo $hex['red'];?
+#12.What is t he output of echo $matrix[1][2];?
+#13. What is the output of echo $people[0]['frstname'];?
+#14. What is tthe output of echo $people[2]['email'];?
+#15. What is he output of var_dump($chunckedArray);?
 ?>
 
 
