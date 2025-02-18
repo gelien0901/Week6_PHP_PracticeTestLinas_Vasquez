07c2bab18857d2221f525d5e059fec62090c13c7
CharleneV-22 <202180273@psu.palawan.edu.ph>
2025-02-18 12:49:31 +0800

    Update practice test 1

--- php practice test.php
+++ php practice test.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+#practice 1
 print_r("Hello");
 print_r([1, 2, 3]);
 
@@ -10,6 +11,15 @@ print_r($person);
 $person = new Person();
 $person = new("Charlene");
 $person = age(25);
+
+#practice 2
+var_dump($Hello);
+var_dump($person)
+var_dump($Hello! Charlene);
+var_dump([1,2,3]);
+
+
 ?>
+
 
 
