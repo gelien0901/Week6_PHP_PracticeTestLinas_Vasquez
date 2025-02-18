5690950f8fcece78e66780e466f5d8f5bbd35963
CharleneV-22 <202180273@psu.palawan.edu.ph>
2025-02-18 12:08:10 +0800

    upload changes

---
+++ Practice_Test 2.php
@@ -0,0 +1,24 @@
+<php
+var_dump($Hello);
+var_dump($person)
+var_dump($Hello! Charlene);
+var_dump([1,2,3]);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ php practice test.php
@@ -0,0 +1,15 @@
+<?php
+print_r("Hello");
+print_r([1, 2, 3]);
+
+class Person {
+    public $name;
+    public $age;
+}
+print_r($person);
+$person = new Person();
+$person = new("Charlene");
+$person = age(25);
+?>
+
+
